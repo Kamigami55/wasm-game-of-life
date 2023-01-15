affb6b2508938f9ff72765ae0beb26c33407cca0
eason_y_chang <eason_y_chang@trendmicro.com>
2023-01-15 13:00:03 +0800

    Implement canvas version

--- src/lib.rs
+++ src/lib.rs
@@ -104,6 +104,18 @@ impl Universe {
     pub fn render(&self) -> String {
         self.to_string()
     }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn cells(&self) -> *const Cell {
+        self.cells.as_ptr()
+    }
 }
 
 impl fmt::Display for Universe {
