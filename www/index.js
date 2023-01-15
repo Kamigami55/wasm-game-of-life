ccac6d5247b4edd535051914525b121419ad585b
eason_y_chang <eason_y_chang@trendmicro.com>
2023-01-15 13:11:02 +0800

    Add FPS display

--- www/index.js
+++ www/index.js
@@ -20,11 +20,43 @@ canvas.width = (CELL_SIZE + 1) * width + 1;
 
 const ctx = canvas.getContext('2d');
 
+var fpsDiv = document.getElementById( 'fpsValue' );
+
+var frames = 0;
+var startTime = performance.now();
+var FPSNormal = 0;
+
+// FPS meter
+// https://www.clicktorelease.com/blog/calculating-fps-with-requestIdleCallback/
+// Every 1000ms, let's update the framerate
+function calculateFPSNormal() {
+	var t = performance.now();
+	var dt = t - startTime;
+
+	// if elapsed time is greater than 1s
+	if( dt > 1000 ) {
+		// calculate the frames drawn over the period of time
+		FPSNormal = frames * 1000 / dt;
+		// and restart the values
+		frames = 0;
+		startTime = t;
+	}
+	frames++;
+}
+
+function updateLabel( fps ) {
+	fpsDiv.textContent = Math.round( fps );
+}
+
 const renderLoop = () => {
   universe.tick();
 
   drawGrid();
   drawCells();
+
+  // We're doing nothing, except updating the framerate
+	calculateFPSNormal();
+	updateLabel( FPSNormal );
 
   requestAnimationFrame(renderLoop);
 };
